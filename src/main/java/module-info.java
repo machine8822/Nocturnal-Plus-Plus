5f39225ea8a4f2404614b89307d45f69c4759990
Gregor Cool <willytk249@gmail.com>
2026-03-03 14:13:47 -0500

    encrpytion

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.nocturnal {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
-
+    requires jbcrypt;
+    
     opens com.nocturnal to javafx.fxml;
     exports com.nocturnal;
     exports com.model;
